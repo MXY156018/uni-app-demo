8c02716ce433e435bc7551b4294a3a3d880aced9
MXY156018 <1132022296@qq.com>
2021-11-12 16:20:17 +0800

    完成商品详情

--- common/api.js
+++ common/api.js
@@ -1,35 +1,42 @@
 import request from "./request.js"
 
-export function getSwiperList(){
+export function getSwiperList() {
 	return request({
-		url:'/api/public/v1/home/swiperdata'
+		url: '/api/public/v1/home/swiperdata'
 	})
 }
-export function getNavList(){
+export function getNavList() {
 	return request({
-		url:'/api/public/v1/home/catitems'
+		url: '/api/public/v1/home/catitems'
 	})
 }
-export function getFloorList(){
+export function getFloorList() {
 	return request({
-		url:'/api/public/v1/home/floordata'
+		url: '/api/public/v1/home/floordata'
 	})
 }
-export function getCateList(){
+export function getCateList() {
 	return request({
-		url:'/api/public/v1/categories'
+		url: '/api/public/v1/categories'
 	})
 }
-export function searchGoods(data){
+export function searchGoods(data) {
 	return request({
-		url:'/api/public/v1/goods/qsearch',
+		url: '/api/public/v1/goods/qsearch',
 		data
 	})
 }
 
-export function getGoods(data){
+export function getGoods(data) {
 	return request({
-		url:'/api/public/v1/goods/search',
+		url: '/api/public/v1/goods/search',
+		data
+	})
+}
+
+export function getGoodDetailApi(data) {
+	return request({
+		url: '/api/public/v1/goods/detail',
 		data
 	})
 }
